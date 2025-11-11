024815ef84560b82458840d057acdec8e6fc7ada
tuananh141101 <email_dang_ky_github_cua_ban>
2025-11-11 17:11:56 +0700

    feat: Add Bird player and continuous scrolling for background/ground

--- Assets/Scripts/BackGroundInfinity.cs
+++ Assets/Scripts/BackGroundInfinity.cs
@@ -13,6 +13,7 @@ public class BackGroundInfinity : MonoBehaviour
     private void Start()
     {
         Debug.Log("Check bgRenderer.material: " + bgRenderer.material);
+        Debug.Log("Check bgRenderer.materiel.mainTextureOffset: " + bgRenderer.material.mainTextureOffset);
     }
 
     void Update()
